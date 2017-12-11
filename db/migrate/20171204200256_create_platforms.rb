8ea2843db6d403c5439989abe979a0edd6fc2e8a
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2017-12-10 23:16:09 -0600

    Move to YAML based seed data

--- db/migrate/20171204200256_create_platforms.rb
+++ db/migrate/20171204200256_create_platforms.rb
@@ -2,6 +2,7 @@ class CreatePlatforms < ActiveRecord::Migration[5.1]
   def change
     create_table :platforms do |t|
       t.string :name
+      t.index :name, unique: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,44 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-platforms = { windows: Platform.find_or_create_by!(name: 'Microsoft Windows'),
-              xbox_one: Platform.find_or_create_by!(name: 'Microsoft Xbox One'),
-              xbox_360: Platform.find_or_create_by!(name: 'Microsoft Xbox 360'),
-              xbox: Platform.find_or_create_by!(name: 'Microsoft Xbox'),
-              ps4: Platform.find_or_create_by!(name: 'Sony Playstation 4'),
-              ps3: Platform.find_or_create_by!(name: 'Sony Playstation 3'),
-              ps2: Platform.find_or_create_by!(name: 'Sony Playstation 2'),
-              ps: Platform.find_or_create_by!(name: 'Sony Playstation'),
-              switch: Platform.find_or_create_by!(name: 'Nintendo Switch'),
-              osx: Platform.find_or_create_by!(name: 'MacOS') }
+seeds_path = Rails.root.join('db', 'seeds')
 
-# This is so we can load our seed images as assets
-#sprocket_env = Sprockets::Environment.new
-#sprocket_env.append_path(Rails.root.join('seed', 'images'))
+platform_seeds = YAML::load_file(File.join(seeds_path, 'platforms.yml'))
+platform_seeds.each do |platform|
+  Platform.find_or_create_by!(name: platform)
+end
 
-games1 = [{ name: 'Forza Motorsport 7', release: Date.parse('October 3, 2017'), developer: 'Turn 10 Studios', publisher: 'Microsoft Studios',
-            platforms: [platforms[:windows], platforms[:xbox_one]], image: 'forza7.jpg' },
-          { name: 'Gran Turismo Sport', release: Date.parse('October 17, 2017'), developer: 'Polyphony Digital', publisher: 'Sony Interactive Entertainment',
-            platforms: [platforms[:ps4]], image: 'gtsport.jpg' },
-          { name: 'Assetto Corsa', release: Date.parse('December 19, 2014'), developer: 'Kunos Simulazioni', publisher: 'Kunos Simulazioni',
-            platforms: [platforms[:windows], platforms[:xbox_one], platforms[:ps4]], image: 'assettocorsa.png' },
-          { name: 'Automobilista', release: Date.parse('February 29, 2016'), developer: 'Reiza Studios', publisher: 'Reiza',
-            platforms: [platforms[:windows]], image: 'automobilista.jpg' }]
-
-cover_image_path = Rails.root.join('seed', 'images', 'games', 'covers')
-
-games1.each do |game_properties|
-  game = Game.find_or_initialize_by(name: game_properties.fetch(:name))
-  game.release = game_properties.fetch(:release)
-  game.developer = game_properties.fetch(:developer)
-  game.publisher = game_properties.fetch(:publisher)
+game_seeds = YAML::load_file(File.join(seeds_path, 'games.yml'))
+game_seeds.each do |game_properties|
+  game = Game.find_or_initialize_by(name: game_properties.fetch('name'))
+  game.release = game_properties.fetch('release')
+  game.developer = game_properties.fetch('developer')
+  game.publisher = game_properties.fetch('publisher')
   game.save!
 
-  game_properties.fetch(:platforms).each do |platform|
-    GamePlatformAssociation.find_or_create_by!(game: game, platform: platform)
+  game_properties.fetch('platforms').each do |platform|
+    GamePlatformAssociation.find_or_create_by!(game: game, platform: Platform.find_by(name: platform))
   end
 
-  image_property = game_properties[:image]
+  image_property = game_properties['image']
 
   # Destroy old image if new image is blank
   if image_property.blank?
@@ -51,6 +33,8 @@ games1.each do |game_properties|
   end
 
   next if image_property.blank?
+
+  cover_image_path = File.join(seeds_path, 'images', 'games', 'covers')
 
   generated_image_properties = Image.properties_from_url(File.join(cover_image_path, image_property))
   image = Image.find_or_initialize_by(id: game.image)
