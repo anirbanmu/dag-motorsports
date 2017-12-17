8cf0b2c3d6a98b9fe628fcda3ee6c46217b45e3d
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2017-12-16 21:20:08 -0600

    Add view spec for show action on games which shows all tracks and circuits

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  let(:game) { create(:game) }
 
   describe "GET #show" do
     it "returns http success" do
-      #get :show
-      #expect(response).to have_http_status(:success)
+      get :show, params: { id: game }
+      expect(response).to have_http_status(:success)
     end
   end
 
