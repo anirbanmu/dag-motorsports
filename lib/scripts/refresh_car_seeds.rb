66f6b0f0709372e26fec4b5b73a49d6a1541ec94
Anirban <anirbanmu@outlook.com>
2017-12-28 15:07:57 -0600

    Add utility script to pull forza 7 car list from the forza forums

---
+++ lib/scripts/refresh_car_seeds.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require_relative '../../config/environment'
+require 'open-uri'
+
+def get_html(url)
+  open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE){|f|f.read}
+end
+
+def update_cars_yml(seed_file, game, cars)
+  game_car_seeds = { game: game }
+  game_car_seeds[:cars] = cars.map{ |car| { name: car } }
+
+  seeds_path = Rails.root.join('db', 'seeds')
+  car_seeds_path = File.join(seeds_path, seed_file)
+  car_seeds = YAML::load_file(car_seeds_path)
+  car_seeds.delete_if{ |o| o[:game] == game }
+  car_seeds.push game_car_seeds
+
+  File.open(car_seeds_path, 'w') {|f| f.write car_seeds.to_yaml }
+end
+
+def refresh_forza
+  html = Nokogiri::HTML(get_html('https://www.forums.forzamotorsport.net/turn10_postst99201_FM7-Car-List.aspx'))
+  post = html.search('.postContentDiv').first
+  cars = post.to_s.scan(/(?:br>|li>)(\d\d\d\d\s+[a-zA-Z]+.+?)[<|▼|❹|❼]/).map { |car| Nokogiri::HTML.parse(car.first.gsub(/\s+/, ' ').strip).text }
+  update_cars_yml('cars.yml', 'Forza Motorsport 7', cars)
+end
+
+refresh_forza
